d5bfcf64635f6773d6a998533dc9ecf5e3b8cbfc
Juan Cruz <jjuancruzrey@gmail.com>
2020-07-15 05:53:01 -0300

    ..

--- src/app/components/resumen/accounts/accounts.component.ts
+++ src/app/components/resumen/accounts/accounts.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsComponent implements OnInit {
 
+  openAcc = false;
+  openMov = false;
+
+  public  movimientos = [{
+    fecha: "30/05",
+    detalle: "Transferencia por CBU",
+    pago: "- $ 270,00",
+    imagen: "arrow-down"
+  }, {
+    fecha: "28/05",
+    detalle: "Crédito por Transferen…",
+    pago: "$ 150,00",
+    imagen: "arrow-up"
+  }, {
+    fecha: "",
+    detalle: "Transferencia por CBU",
+    pago: "- $ 230,50",
+    imagen: "arrow-down"
+  }, {
+    fecha: "26/05",
+    detalle: "Crédito por Transferen…",
+    pago: "$ 480,00",
+    imagen: "arrow-up"
+  }];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/resumen/cards/cards.component.ts
+++ src/app/components/resumen/cards/cards.component.ts
@@ -7,27 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
-  public  cards = [{
-  path: "./assets/png/tarjeta_2.png",
-  number: "#xxxx - 4567",
-  pago: "Últimos Consumos",
-  cierra: "12/11",
-  vence: "05/12",
-  pesos: "14.500",
-  dolares: "45"
-}, {
-  path: "./assets/png/tarjeta_2.png",
-  number: "#xxxx - 4567",
-  pago: "Últimos Consumos",
-  cierra: "12/11",
-  vence: "05/12",
-  pesos: "14.500",
-  dolares: "45"
-}];
+  public cards = [{
+    path: "./assets/png/tarjeta_2.png",
+    number: "#xxxx - 4567",
+    pago: "Últimos Consumos",
+    cierra: "12/11",
+    vence: "05/12",
+    pesos: "14.500",
+    dolares: "45",
+  }, {
+    path: "./assets/png/tarjeta_2.png",
+    number: "#xxxx - 4567",
+    pago: "Últimos Consumos",
+    cierra: "12/11",
+    vence: "05/12",
+    pesos: "14.500",
+    dolares: "45",
+  }, {
+    path: "./assets/png/tarjeta_2.png",
+    number: "#xxxx - 4567",
+    pago: "Últimos Consumos",
+    cierra: "12/11",
+    vence: "05/12",
+    pesos: "14.500",
+    dolares: "45",
+  }, {
+    path: "./assets/png/tarjeta_2.png",
+    number: "#xxxx - 4567",
+    pago: "Últimos Consumos",
+    cierra: "12/11",
+    vence: "05/12",
+    pesos: "14.500",
+    dolares: "45",
+  }];
+  constructor() { }
 
-constructor() { }
+  ngOnInit(): void {
 
-ngOnInit(): void {
-}
+  }
 
 }
