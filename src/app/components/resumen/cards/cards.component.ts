63146d077b1f4ca9996e32cc6d58730a033b1f60
Juan Cruz <jjuancruzrey@gmail.com>
2020-07-17 07:09:13 -0300

    cards issue

--- src/app/components/resumen/cards/cards.component.ts
+++ src/app/components/resumen/cards/cards.component.ts
@@ -17,6 +17,21 @@ export class CardsComponent implements OnInit {
     dolares: "45",
     valid: true,
   }, {
+    path: "./assets/png/masterCard.png",
+    number: "#xxxx - 4567",
+    pago: "Últimos Consumos",
+    cierra: "12/11",
+    vence: "05/12",
+    pesos: "3.800",
+    dolares: "0",
+    valid: true,
+  },{
+    valid:false,
+  },{
+    valid: false,
+  }];
+
+  public invalid = [{
     path: "./assets/png/tarjeta_2.png",
     number: "#xxxx - 4567",
     pago: "Últimos Consumos",
@@ -25,11 +40,8 @@ export class CardsComponent implements OnInit {
     pesos: "14.500",
     dolares: "45",
     valid: true,
-  },{
-    valid:false,
-  },{
-    valid: false,
   }];
+
   constructor() { }
 
   ngOnInit(): void {
